bff1afd8b6145f1349b3cce5e56846654c46e51d
bhashi2 <bhashi2@uic.edu>
2023-05-20 13:38:55 -0500

    functions added

---
+++ functions.py
@@ -0,0 +1,23 @@
+import os
+import pyautogui as pyg
+import time
+from passwords import passMap
+
+#opens league client
+#waits for it to open (maybe better way?)
+#first moveto is to username, 2nd is pass and finally clicks on submit
+def start_and_login(name):
+    os.startfile ("C:\Riot Games\League of Legends\LeagueClient.exe")
+    time.sleep(5)
+    pyg.moveTo(700, 775)
+    pyg.leftClick()
+    pyg.write(name)
+    pyg.moveTo(700, 900)
+    pyg.leftClick()
+    pyg.write(passMap[name])
+    pyg.moveTo(930, 1555)
+    pyg.leftClick()
+
+def switch_account(name):
+    return None
+
